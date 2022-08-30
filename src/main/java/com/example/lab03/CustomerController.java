da3ac9a15b024ff4781a5d878a5a0fe8ecc66d93
chai <chaiklz@hotmail.com>
2022-08-30 11:19:42 +0700

    edit

--- src/main/java/com/example/lab03/CustomerController.java
+++ src/main/java/com/example/lab03/CustomerController.java
@@ -1,42 +1,82 @@
 package com.example.lab03;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 @RestController
 public class CustomerController {
-    private List<Customer> customers = new ArrayList<>();
+    private List<Customer> customers;
 
-    @RequestMapping(value = "/customers", method = RequestMethod.GET)
-    public int getCustomers(){
+    public CustomerController(){
+        customers = new ArrayList<>();
         customers.add(new Customer("1010","John",true,25));
         customers.add(new Customer("1018","Peter",true,24));
         customers.add(new Customer("1019","Sara",false,23));
         customers.add(new Customer("1110","Rose",false,23));
         customers.add(new Customer("1001","Emma",false,30));
-        return  customers.size();
+    }
+    @RequestMapping(value = "/customers", method = RequestMethod.GET)
+    public List<Customer> getCustomers(){
+        return  customers;
     }
     @RequestMapping(value = "/customersbyid/{id}", method = RequestMethod.GET)
     public Customer getCustomerByID (@PathVariable("id") String ID){
-        return customers.get(0);
+        int index = 0;
+        for(int i = 0; i < customers.size();i++){
+            if(ID.equals(customers.get(i).getID())){
+                index = i;
+            }
+        }
+        return customers.get(index);
     }
     @RequestMapping(value = "/customersbyname/{n}", method = RequestMethod.GET)
-    public Customer getCustomerByName (@PathVariable("n") String ID){
-        return customers.get(0);
+    public Customer getCustomerByName (@PathVariable("n") String name){
+        int index = 0;
+        for(int i = 0; i < customers.size();i++){
+            if(name.equals(customers.get(i).getName())){
+                index = i;
+            }
+        }
+        return customers.get(index);
     }
     @RequestMapping(value = "/customersDelbyid/{id}", method = RequestMethod.DELETE)
     public boolean delCustomerByID (@PathVariable("id") String ID){
+        int index = 0;
+        for (int i = 0; i < customers.size();i++){
+            if(ID.equals(customers.get(i).getID())){
+                index = i;
+            }
+        }
+        customers.remove(index);
+        for (int i = 0; i < customers.size();i++){
+            if(!ID.equals(customers.get(i).getID())){
+                return true;
+            }
+        }
         return  false;
     }
     @RequestMapping(value = "/customersDelbyname/{n}", method = RequestMethod.DELETE)
     public boolean delCustomerByName (@PathVariable("n") String name){
+        int index = 0;
+        for (int i = 0; i < customers.size();i++){
+            if(name.equals(customers.get(i).getName())){
+                index = i;
+            }
+        }
+        customers.remove(index);
+        for (int i = 0; i < customers.size();i++){
+            if(!name.equals(customers.get(i).getName())){
+                return true;
+            }
+        }
         return  false;
     }
-    public boolean addCustomer(String ID, String n, String s, int a){
+    @RequestMapping(value = "/addCustomer2", method = RequestMethod.POST)
+    public boolean addCustomer(@RequestParam("id") String ID,
+                               @RequestParam("name")String n,
+                               @RequestParam("sex") String s,
+                               @RequestParam("age") int a){
         boolean b = false;
         if(s == "Female" ^ s == "female"){
             b = false;
