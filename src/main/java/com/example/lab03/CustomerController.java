fc84af185fd270b188f7743a7d5004e6be9a91d7
chai <chaiklz@hotmail.com>
2022-08-30 11:23:19 +0700

    edit

--- src/main/java/com/example/lab03/CustomerController.java
+++ src/main/java/com/example/lab03/CustomerController.java
@@ -6,10 +6,9 @@ import java.util.ArrayList;
 import java.util.List;
 @RestController
 public class CustomerController {
-    private List<Customer> customers;
+    private List<Customer> customers = new ArrayList<>();
 
     public CustomerController(){
-        customers = new ArrayList<>();
         customers.add(new Customer("1010","John",true,25));
         customers.add(new Customer("1018","Peter",true,24));
         customers.add(new Customer("1019","Sara",false,23));
